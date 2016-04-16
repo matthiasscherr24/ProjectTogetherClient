f86cf67bedc00bf3de66968ed0d874748eb6ce3f
Matthias Scherr <matthias.scherr@app-access.de>
2016-04-16 22:26:09 +0200

    LoginActivity

---
+++ ProjectTogetherClient15_30_16thApril/app/src/main/java/org/projecttogether/hackathon/projecttogetherclient15_30_16thapril/Idea.java
@@ -0,0 +1,13 @@
+package org.projecttogether.hackathon.projecttogetherclient15_30_16thapril;
+
+
+ /**
+ * Created by Max on 16.04.2016.
+ */
+public class Idea {
+    String text, requirement[], tags[], location;
+    int category;
+}
+
+
+
