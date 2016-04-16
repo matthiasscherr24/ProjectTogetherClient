04119091ce3cb4e0894205b406d3a16cad019b2b
Max Beierl <maxbeierl@gmail.com>
2016-04-16 19:14:07 +0200

    User class created

---
+++ ProjectTogetherClient15_30_16thApril/app/src/main/java/org/projecttogether/hackathon/projecttogetherclient15_30_16thapril/User.java
@@ -0,0 +1,33 @@
+package org.projecttogether.hackathon.projecttogetherclient15_30_16thapril;
+
+/**
+ * Created by Max on 16.04.2016.
+ */
+public class User {
+    int id, indexSkills;
+    String username, email, password, skills[];
+    boolean interests;
+
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        interests = new boolean[10];
+        skills = String[20];
+        indexSkills = 0;
+    }
+
+    public void setSkills(String newSkill){
+        if(indexSkills < 20){
+            skills[indexSkills] = newSkill;
+            indexSkills++;
+        }
+        // exeption mehr als 20 Skills
+    }
+
+    public void setInterests(boolean interests[]){
+        this.interests = interests;
+    }
+
+
+}
